6a410bf937aba8e87615d3587c223c862b653106
YashKapoor1004 <yashkapoor1004@gmail.com>
2021-01-17 05:51:45 -0500

    data

---
+++ public/interior/data/data.js
@@ -0,0 +1,5 @@
+document.querySelector('.topBar').addEventListener('mouseover', function(event){
+    document.querySelector('.jan17Div').style.display = 'block';
+    document.querySelector('.horLine').style.display = 'block';
+    document.querySelector('.noFuel').style.display = "none";
+})
